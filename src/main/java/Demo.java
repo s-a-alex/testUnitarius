c19d4093457b9337a5c4d65218d3df8f53ea8cf3
s-a-alex <s-a-alex@yandex.ru>
2022-02-22 14:55:18 +0300

    alex

--- src/main/java/Demo.java
+++ src/main/java/Demo.java
@@ -1,41 +1,19 @@
-import java.nio.charset.Charset;
-import java.util.ArrayList;
-import java.util.Arrays;
-
 public class Demo {
-//    final static int N = 63;
-//    final static int M = 48;
 
-//    public static void main(String[] str) {
-//        Integer[] array = new Integer[N];
-//        for (int i = 0; i < N; i++) array[i] = i;
-//        int start = 0, m = M, n = N, k = n - m;
-//        new Demo().run(array, start, m, n, k);
-//    }
-
-    public void run(Integer[] array, int m) {
-        int current=array[0], mCurrent=m;
-        for (int i=0;i<array.length;i++) {
-            array[mCurrent]=
-            start = nRun - (m > k ? n % k : m);
-            m = nRun - (m > k ? (k + n % k)-(k + n % k) / (n % k) * (n % k) : n-n / m * m) - start;
-            n = nRun - start;
-            k = n - m;
-        }
-//        System.out.println(Arrays.asList(array));
-    }
-
-    private int range(Integer[] array, int start, int m, int n, int k) {
-        int tmp0, tmp1;
-        for (int i = start + m; i < start + n; i++) {
-            tmp0 = array[i];
-            for (int j = 0; j < n / k; j++) {
-                tmp1 = array[i - m + j * k];
-                array[i - m + j * k] = tmp0;
-                tmp0 = tmp1;
+    public <T> void run(T[] array, int shift) {
+        int start = 0, pos = start + shift;
+        T valInsert = array[start];
+        for (int i = 0; i < array.length; i++) {
+            T valSave = array[pos];
+            array[pos] = valInsert;
+            if (pos != start) {
+                valInsert = valSave;
+                pos = (pos + shift >= array.length ? pos + shift - array.length : pos + shift);
+            } else {
+                start++;
+                valInsert = array[start];
+                pos = start + shift;
             }
-            if (!(m >= k && n % k == 0)) array[i] = tmp0;
         }
-        return (m > k ? (n % k == 0 ? 0 : k % (n % k)) : n % m);
     }
 }
--- src/test/java/TestDemo.java
+++ src/test/java/TestDemo.java
@@ -1,30 +1,32 @@
-import org.junit.Before;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.util.Arrays;
 import java.util.Random;
+
 import static org.junit.Assert.assertEquals;
 
+@Slf4j
 public class TestDemo {
-    private static Logger logger = LoggerFactory.getLogger(TestDemo.class);
+
+    private final static int maxM = 1024;
+    private final static int cntTest = 1000;
 
     @Test
     public void testRun() {
-        logger.info("testRun");
-        Random random=new Random();
-        Demo demo=new Demo();
-        for(int j=0;j<100;j++) {
-            int start = 0, m = random.nextInt(1024) + 1, k = random.nextInt(1024) + 1, n = m + k;
-            Integer[] array = new Integer[n], arrayTest = new Integer[n];
-            for (int i = 0; i < n; i++) array[i] = i;
-            for (int i = 0; i < n; i++) arrayTest[i] = array[(i + m) % n];
-            logger.info(String.format("m=%d, k=%d, n=%d", m, k, n));
-            logger.info(String.valueOf(Arrays.asList(arrayTest)));
-            demo.run(array, start, m, n, k);
-            logger.info(String.valueOf(Arrays.asList(array)));
-            for (int i = 0; i < n; i++) assertEquals(array[i], arrayTest[i]);
+        log.info("testRun");
+        Random random = new Random();
+        Demo demo = new Demo();
+        for (int j = 0; j < cntTest; j++) {
+            int m = random.nextInt(maxM) + 1;
+            Integer[] array = new Integer[m + random.nextInt(maxM) + 1], arrayTest = new Integer[array.length];
+            for (int i = 0; i < array.length; i++) array[i] = i;
+            for (int i = 0; i < array.length; i++) arrayTest[i] = array[(i + m) % array.length];
+            log.info(String.format("m=%d, k=%d, n=%d", m, array.length - m, array.length));
+            log.info(String.valueOf(Arrays.asList(arrayTest)));
+            demo.run(array, array.length - m);
+            log.info(String.valueOf(Arrays.asList(array)));
+            for (int i = 0; i < array.length; i++) assertEquals(array[i], arrayTest[i]);
         }
     }
 }
